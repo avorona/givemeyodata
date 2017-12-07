80e87ffd4e630e97a3d2151606ae0368e6efa198
AVorona <vitalii.parshikov@gmail.com>
2017-12-07 23:02:20 +0200

    git commit -m "Demo"

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const cool = require('cool-ascii-faces');
+const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/cool', (req, response) => response.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//   app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
